c97821bb2d401bf52ba48a92c168eeec9a7750ea
Rasadul Islam <rasadulislam.cse@gmail.com>
2025-01-14 02:34:59 +0600

    added error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            I am from Error Page
+        <div className='flex flex-col justify-center items-center w-3/4 mx-auto my-20 bg-gray-300 p-5 rounded-xl'>
+            <h1 className='text-7xl font-extrabold text-red-400 my-5'>
+                Oops!
+            </h1>
+            <h2 className='text-4xl my-2'>404 - page not found</h2>
+            <p className='my-2'>The page you are looking for might have been removed <br/> had its name changed or is temporarily unavailable</p>
+            <Link to='/' className='btn uppercase font-bold text-lg mt-5 bg-lime-300 border-none'>Go to homepage</Link>
         </div>
     );
 };
